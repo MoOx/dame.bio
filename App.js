7b50cf1080104191983ec5abebe5564a71b08c9f
Maxime Thirouin <git@moox.io>
2018-12-09 19:04:38 +0100

    Enable pre-rendering for index & paginated index

--- App.js
+++ App.js
@@ -4,11 +4,49 @@ import { AppRegistry } from "react-native-web";
 import { createApp } from "@phenomic/preset-react-app/lib/client";
 import { createContainer } from "@phenomic/preset-react-app/lib/es6/src/phenomicPresetReactApp.bs.js";
 
+import gql from "graphql-tag";
+import initApollo from "./src/init-apollo.js";
 import RoutePosts from "./lib/es6/src/components/RoutePosts.bs.js";
 import RoutePost from "./lib/es6/src/components/RoutePost.bs.js";
 
-RoutePosts.getAllPossibleUrls = () => {
-  return ["/"];
+let apolloClient = initApollo();
+
+RoutePosts.getAllPossibleUrls = async ({ path }) => {
+  if (path == "/") {
+    return ["/"];
+  }
+  if (path == "/after/:after") {
+    return apolloClient
+      .query({
+        query: gql`
+          {
+            posts(first: 1000, where: { status: PUBLISH }) {
+              edges {
+                cursor
+              }
+            }
+          }
+        `
+      })
+      .then(({ data }) => {
+        // console.log(`received data ${JSON.stringify(data, null, 2)}`);
+        return data.posts.edges
+          .map(item => {
+            try {
+              return "/after/" + item.cursor + "/";
+            } catch (e) {
+              console.log(`received error ${e}`);
+              console.log(JSON.stringify(item, null, 2));
+            }
+          })
+          .filter(i => i);
+      })
+      .catch(error => {
+        console.log(`received error ${error}`);
+        return [];
+      });
+  }
+  return [];
 };
 
 RoutePost.getAllPossibleUrls = () => {
