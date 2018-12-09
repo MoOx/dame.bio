58b5c54d3322f41f0d7a839586b9f493cc6a5bea
Maxime Thirouin <git@moox.io>
2018-12-09 17:36:22 +0100

    Fix static hydratation phase by loading RNW properly on the client

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Router, Route, browserHistory } from "react-router";
+import { AppRegistry } from "react-native-web";
 import { createApp } from "@phenomic/preset-react-app/lib/client";
 import { createContainer } from "@phenomic/preset-react-app/lib/es6/src/phenomicPresetReactApp.bs.js";
 
@@ -14,16 +15,23 @@ RoutePost.getAllPossibleUrls = () => {
   return [];
 };
 
-export default createApp(() => (
+const routes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={RoutePosts} />
     <Route path="/after/:after" component={RoutePosts} />
     <Route path="/:cat/*" component={RoutePost} />
   </Router>
-));
+);
+
+const render = (rootComponent, rootTag) => {
+  AppRegistry.registerComponent("App", () => () => rootComponent);
+  AppRegistry.runApplication("App", { rootTag });
+};
+
+export default createApp(routes, render);
 
 if (module.hot) {
-  module.hot.accept(() => renderApp(routes));
+  module.hot.accept(() => renderApp(routes, render));
 }
 
 // it's that easy to have for initial loading ?
