5f833497eb80a5883cbcd5188bdc1a17c57d176c
Maxime Thirouin <git@moox.io>
2018-12-21 23:33:21 +0100

    Add touchable opacity link

---
+++ src/components/TouchableOpacityLink.js
@@ -0,0 +1,66 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import cx from "classnames";
+import { TouchableOpacity, Linking } from "react-native";
+
+import {
+  handleClick,
+  handleKeyDown,
+  handleEvent,
+  isActive,
+} from "@phenomic/plugin-renderer-react/lib/components/Link.js";
+
+const BASENAME = process.env.PHENOMIC_APP_BASENAME || "/";
+
+export const origin = (
+  url, // jsdom can return "null" string...
+) =>
+  (url.origin !== "null" && url.origin) || // // IE does not correctly handle origin, maybe Edge does...
+  url.protocol + "//" + url.hostname + (url.port ? ":" + url.port : "");
+const isSameOrigin = url =>
+  origin(url) === origin(window.location) &&
+  url.pathname.indexOf(BASENAME) > -1;
+
+export const handlePress = (props, router) => event => {
+  handleEvent(props, router)(event);
+  // fix for outgoing links
+  if (!event.preventDefaulted && !isSameOrigin(event.currentTarget)) {
+    const href = event.currentTarget.href;
+    Linking.canOpenURL(href).then(supported => {
+      if (supported) {
+        Linking.openURL(href);
+      } else {
+        alert("Ouppps. Impossible d'ouvrir le lien!");
+      }
+    });
+  }
+};
+
+function Link(props, context) {
+  const { to, style, activeStyle, ...otherProps } = props;
+  const href = to || props.href || "";
+
+  return (
+    <TouchableOpacity
+      {...otherProps}
+      accessibilityRole="link"
+      href={
+        href.indexOf("://") > -1
+          ? href
+          : href.charAt(0) === "/"
+            ? BASENAME + href.slice(1)
+            : href
+      }
+      onPress={handlePress(props, context.router)}
+      style={[style, isActive(href, context) && activeStyle]}
+    />
+  );
+}
+
+Link.contextTypes = {
+  router: PropTypes.object.isRequired,
+};
+
+Link.displayName = "TouchableOpacityLink";
+
+export default Link;
---
+++ src/components/TouchableOpacityLink.re
@@ -0,0 +1,39 @@
+[@bs.module "../../../../src/components/TouchableOpacityLink"]
+external reactClass: ReasonReact.reactClass = "default";
+
+[@bs.deriving abstract]
+type jsProps = {
+  [@bs.optional]
+  href: string,
+  [@bs.optional]
+  style: BsReactNative.Style.t,
+  [@bs.optional]
+  activeStyle: BsReactNative.Style.t,
+  [@bs.optional]
+  onMouseEnter: unit => unit,
+  [@bs.optional]
+  onMouseLeave: unit => unit,
+};
+
+let make =
+    (
+      ~href=?,
+      ~style=?,
+      ~activeStyle=?,
+      ~onMouseEnter=?,
+      ~onMouseLeave=?,
+      children,
+    ) =>
+  ReasonReact.wrapJsForReason(
+    ~reactClass,
+    ~props=
+      jsProps(
+        ~href?,
+        ~style?,
+        ~activeStyle?,
+        ~onMouseEnter?,
+        ~onMouseLeave?,
+        (),
+      ),
+    children,
+  );
