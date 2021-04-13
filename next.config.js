ebd0d38e64548e603d07d29d23262fe74a3b6515
Maxime Thirouin <git@moox.io>
2021-04-13 19:13:57 +0200

    Use next-transpile-modules
    Ref https://github.com/martpie/next-transpile-modules/issues/170#issuecomment-818864712

--- next.config.js
+++ next.config.js
@@ -2,13 +2,14 @@ const path = require("path");
 const bsconfigJson = require("./bsconfig.json");
 const packageJson = require("./package.json");
 const modulesToTranspile = [
-  "bs-platform",
-  ...bsconfigJson["bs-dependencies"],
+  // rescript & rescript deps
+  "bs-platform/package.json",
+  ...bsconfigJson["bs-dependencies"].map((d) => d + "/package.json"),
   ...Object.keys(packageJson.dependencies).filter((dep) =>
     dep.startsWith("react-native")
   ),
 ];
-// const withTM = require("next-transpile-modules")(modulesToTranspile);
+const withTM = require("next-transpile-modules")(modulesToTranspile);
 
 const config = {
   images: {
@@ -20,7 +21,7 @@ const config = {
   },
   // target: "serverless",
 
-  // bs-platform
+  // rescript
   pageExtensions: ["jsx", "js", "bs.js"],
 
   webpack: (config, options) => {
@@ -45,56 +46,6 @@ const config = {
       ...config.resolve.extensions,
     ];
 
-    // logic below for externals has been extracted from "next-transpile-modules"
-    // we won't use this modules as they don't allow package without "main" field...
-    // https://github.com/martpie/next-transpile-modules/issues/170
-    const getPackageRootDirectory = (m) =>
-      path.resolve(path.join(__dirname, "node_modules", m));
-    const modulesPaths = modulesToTranspile.map(getPackageRootDirectory);
-
-    const hasInclude = (context, request) => {
-      return modulesPaths.some((mod) => {
-        // If we the code requires/import an absolute path
-        if (!request.startsWith(".")) {
-          try {
-            const moduleDirectory = getPackageRootDirectory(request);
-            if (!moduleDirectory) return false;
-            return moduleDirectory.includes(mod);
-          } catch (err) {
-            return false;
-          }
-        }
-        // Otherwise, for relative imports
-        return path.resolve(context, request).includes(mod);
-      });
-    };
-    config.externals = config.externals.map((external) => {
-      if (typeof external !== "function") return external;
-      // if (isWebpack5) {
-      //   return async ({ context, request, getResolve }) => {
-      //     if (hasInclude(context, request)) return;
-      //     return external({ context, request, getResolve });
-      //   };
-      // }
-      return (context, request, cb) => {
-        return hasInclude(context, request)
-          ? cb()
-          : external(context, request, cb);
-      };
-    });
-
-    config.module.rules.push({
-      test: /\.js$/,
-      use: options.defaultLoaders.babel,
-      include: modulesPaths,
-    });
-
-    // {
-    //   test: /\.bs\.js$/,
-    //   use: options.defaultLoaders.babel,
-    //   include: /node_modules/,
-    // },
-
     // avoid duplicated react
     // config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
 
@@ -102,5 +53,4 @@ const config = {
   },
 };
 
-// module.exports = withTM(config);
-module.exports = config;
+module.exports = withTM(config);
